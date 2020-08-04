39baf21080ee2890dae70888c6f25bd8a4b93957
Namrata <r.namrata99@gmail.com>
2020-08-04 23:55:33 +0530

    React Redux Form Revised

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Input } from 'reactstrap';
 import {Link} from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,68 +14,16 @@ class Contact extends Component {
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     firstname: '', lastname: '', telnum: '', email: '', agree: false, contactType: 'Tel. ',message: '',
-        //     touched: { firstname: false, lastname: false, telnum: false, email: false }
-        // }
-
-
         this.handleSubmit = this.handleSubmit.bind(this);
-        // this.handleInputChange = this.handleInputChange.bind(this);
-        // this.handleBlur = this.handleBlur.bind(this);
     }
-
-    // handleInputChange(event) {
-    //     const target = event.target;
-    //     const value = target.type === 'checkbox' ? target.checked : target.value;
-    //     const name = target.name;
-
-    //     this.setState ({
-    //         [name]: value
-    //     })
-    // }
 
     handleSubmit(values) {
         console.log("Current State is: " + JSON.stringify(values));
         alert("Current State is: " + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
-    // handleBlur = (field) => (evt) => {
-    //     this.setState({
-    //         touched: { ...this.state.touched, [field]: true }
-    //     });
-    // }
-
-    // validate(firstname, lastname, telnum, email) {
-    //     const errors = {
-    //         firstname: '',
-    //         lastname: '',
-    //         telnum: '',
-    //         email: ''
-    //     };
-
-    //     if(this.state.touched.firstname && firstname.length < 3 )
-    //         errors.firstname = "First Name should be >= 3 characters";
-    //     else if( this.state.touched.firstname && firstname.length > 10 )
-    //         errors.firstname = "First Name should be <= 10 characters";
-
-    //     if(this.state.touched.lastname && lastname.length < 3 )
-    //         errors.lastname = "Last Name should be >= 3 characters";
-    //     else if( this.state.touched.lastname && lastname.length > 10 )
-    //         errors.lastname = "Last Name should be <= 10 characters";
-
-    //     const reg = /^\d+$/; // should be all numbers
-    //     if(this.state.touched.telnum && !reg.test(telnum))
-    //         errors.telnum = "Tel. Number should only contain numbers";
-        
-    //     if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
-    //         errors.email = "Email should contain a '@' sign";
-
-    //     return errors;
-    // }
-
     render () {
-        // const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
         return (
             <div className="container">
                 <div className="row">
@@ -119,7 +67,7 @@ class Contact extends Component {
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -236,7 +184,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
